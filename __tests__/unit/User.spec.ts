9bde2257a3c8d011d676685c10f4e91411c8a784
Vinicius Cainelli <vicainelli+github@gmail.com>
2021-10-03 21:45:37 +0200

    Remove unused test

--- __tests__/unit/User.spec.ts
+++ __tests__/unit/User.spec.ts
@@ -12,14 +12,4 @@ test.group('User', () => {
 
     assert.notEqual(user.password, 'secret')
   })
-  test('ensure user is not verified by default', async (assert) => {
-    const user = new User()
-    user.email = faker.internet.email()
-    user.name = faker.internet.userName()
-    user.password = 'secret'
-    await user.save()
-    await user.refresh()
-
-    assert.equal(user.verified, false)
-  })
 })
